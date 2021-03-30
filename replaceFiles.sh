698290e6bfda28d8b97f9be87703c2ca11814cef
l-gorman <lgorman@turing.ac.uk>
2021-03-30 08:26:12 +0000

    finalising replacement script

--- replaceFiles.sh
+++ replaceFiles.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 cp default /etc/nginx/sites-available/default
+
+cp legume_choice_data_process.service /etc/systemd/system/legume_choice_data_process.service
+
+systemctl restart nginx
+
+systemctl restart legume_choice_data_process.service
+
+systemctl daemon-reload
